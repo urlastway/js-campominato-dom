830b99ef2c2cc906e1884dbbee31c4c88edb48d3
urlastway <andriy.semenchenko13@gmail.com>
2022-05-10 20:31:16 +0200

    correct

--- main.js
+++ main.js
@@ -4,12 +4,30 @@ function getRndInteger(min, max) {
 
 var pcNumer = [];
 var userNum = [];
+var check = true;
+let level = 0;
 
-for(let i = 0; i<16; i++){
-    var pcGenerateNumber = getRndInteger(1, 100);
+do{
+
+var choseLeveL = Number(prompt("inserisci il livello di gioco da 1 a 3"))
+
+}while(choseLeveL > 3 || choseLeveL < 1);
+
+if(choseLeveL == 1){
+  level = 100;
+}
+else if(choseLeveL == 2){
+  level = 80;
+}
+else{
+  level = 60;
+}
+
+while (pcNumer.length < 16) {
+    var pcGenerateNumber = getRndInteger(1, level);
       if(pcNumer.includes(pcGenerateNumber)){
         do{
-          pcGenerateNumber = Math.floor(Math.random() * 101);
+          pcGenerateNumber = Math.floor(Math.random() * (level + 1));
         }while(pcNumer.includes(pcGenerateNumber));
         pcNumer.push(pcGenerateNumber);
       }
@@ -21,25 +39,28 @@ for(let i = 0; i<16; i++){
 
 console.log(pcNumer);
 
-for(let i = 0; i<16; i++){
-  var userNumber = Number(prompt("Inserisci un numero"));
+while (userNum.length < 7 && check == true){
+  do{
+
+    var userNumber = Number(prompt("Inserisci un numero tra 0 e 100"))
+
+  }while(userNumber >= level || userNumber <= 0);
+
       if(userNum.includes(userNumber)){
         do{
           userNumber = Number(prompt("Hai inserito un numero già inserito, riprova"));
         }while(userNum.includes(userNumber));
-        userNum.push(userNumber);
         console.log(userNumber);
+        userNum.push(userNumber);
       }
       else if (pcNumer.includes(userNumber)) {
         console.log("numero vietato, hai perso");
-        break;
+        check = false;
       }
-      else if(i < 15){
-        userNum.push(userNumber);
+      else {
         console.log(userNumber);
+        userNum.push(userNumber);
       }
-      else{
-        console.log(userNumber + " " + "Hai vinto")
-      }
-      userNum.push(userNumber);
 };
+
+console.log(`Il tuo punteggio è ${userNum.length}`);
